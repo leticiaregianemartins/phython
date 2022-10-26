574d81810633893f6a0de3e79065f7b95f38c7a1
leticiaregianemartins <letregianemartins@gmail.com>
2022-10-25 21:42:27 -0300

    exercicio2

---
+++ exercicio2.py
@@ -0,0 +1,6 @@
+# Crie um programa para receber valores em variáveis de string einteiro e mostre os valores ao final.
+# Receber valores o comando é o: input()
+# Converter para um inteiro o comando é o: int()
+# Converter para um real o comando é o: float()
+a = input("digite um numero inteiro" )
+b = input("digite um número inteiro" )
