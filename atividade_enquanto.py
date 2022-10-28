17745b495212624c40a9bfc7edd90b9100ac7f81
leticiaregianemartins <letregianemartins@gmail.com>
2022-10-27 21:51:29 -0300

    atividade_enquanto

---
+++ atividade_enquanto.py
@@ -0,0 +1,31 @@
+# Crie um programa de calculadora que realiza as operações de
+# soma, multiplicação, divisão e subtração, será perguntado ao
+# usuário se deseja continuar com o uso da calculadora, enquanto ele digitar (“S” – Sim) o programa irá reiniciar a calculadora.
+# • Repetição usando ”ENQUANTO”
+# while == “valor”
+# #sendo verdadeiro repete até ser falso
+
+num1 = int(input("digite o primeiro numero"))
+num2 = int(input("digite o segundo numero"))
+
+while == "sim"
+resultado = num1 + num2
+print("o resultado é", resultado)
+
+resultado = num1 - num2
+print("o resultado é", resultado)
+
+resultado = num1 * num2
+print("o resultado é", resultado)
+
+resultado = num1 / num2
+print("o resultado é", resultado)
+
+continuar = input("sim")
+print("deseja continuar", continuar)
+
+
+
+
+
+
