d008a55ee449f0634b5cf6aa3fc08e966b703e25
leticiaregianemartins <letregianemartins@gmail.com>
2022-10-28 21:43:32 -0300

    atividade_matriz

---
+++ atividade_matriz.py
@@ -0,0 +1,8 @@
+# Crie um programa em que o usuário deve digitar números
+# inteiros para uma matriz de 5 linhas e 5 colunas .
+# • Após a digitação dos números, o usuário deve digitar um número aleatório e verificar se ele se encontra na matriz.
+# • Ao final, os índices da linha e da coluna devem ser impressos se o elemento for encontrado;
+# caso contrário, a mensagem “elemento não encontrado” deve ser mostrada na tela.
+
+matriz = [[1,2,3,4], [1,2,3,4], [1,2,3,4], [1,2,3,4], [1,2,3,4]]
+
