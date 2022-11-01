edded6452429d7c76eed2671ae157d7f2b1b0a9b
leticiaregianemartins <letregianemartins@gmail.com>
2022-10-31 21:48:23 -0300

    atividade_enem

---
+++ atividade_enem.py
@@ -0,0 +1,17 @@
+# Crie um programa que leia um conjunto de nomes de 20 estudantes
+# inscritos na prova do ENEM. Com esses nomes, realizar uma
+# ordenação crescente usando uma função para facilitar a localização
+# do nome na lista que será afixada no quadro de avisos da escola.
+# def nomeDaFuncao():
+# #linhas de comando da função
+# #Se tiver parâmetros é só colocar no parênteses (parm1, param2)
+# Para chamar a função é
+# nomeDaFuncao()
+# #se tiver que passar parâmetros é só colocar no parêntese (param1, param2)
+
+lista_nomesenem = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,]
+
+lista_nomesenem = str(input("digites os nomes dos estudantes"))
+
+for i in range(1, 20):
+    lista_nomesenem
