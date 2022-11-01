08c610d91be5388596c0b1ac4818d7eb6d4c6d1b
leticiaregianemartins <letregianemartins@gmail.com>
2022-10-31 21:31:26 -0300

    atividade_multiplicaçãodedois

--- atividade_multiplicaçãodedois.py
+++ atividade_multiplicaçãodedois.py
@@ -8,14 +8,16 @@
 # #se tiver que passar parâmetros é só colocar no parêntese (param1, param2)
 
 
-def multiplicacao(resultado = n1 * n2):
-    return(resultado)
+def multiplicacao(n1, n2):
+    resultado = n1 * n2
+    return resultado
 
 
-n1 = int(input("digite o 1 número"))
-n2 = int(input("digite o 2 número"))
-resultado = multiplicacao(n1, n2):
-print("o resultado é", resultado)
+n1 = int(input("digite o 1 número "))
+n2 = int(input("digite o 2 número "))
+
+print("o resultado",n1, "*", n2, "=", multiplicacao(n1, n2))
+
 
 
 
